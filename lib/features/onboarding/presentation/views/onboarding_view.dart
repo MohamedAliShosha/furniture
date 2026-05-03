f45c3deebb2ac705c1ba668d8516ba6b57a3dcc4
mohamed <mohamedaliaboshosha927@gmail.com>
2026-05-03 14:54:16 -0700

    chore(onboarding): remove unused gap import

--- lib/features/onboarding/presentation/views/onboarding_view.dart
+++ lib/features/onboarding/presentation/views/onboarding_view.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:gap/gap.dart';
 import 'package:go_router/go_router.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import '../../../../core/utils/app_router.dart';
