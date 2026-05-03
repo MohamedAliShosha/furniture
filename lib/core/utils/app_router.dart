00fa4704a163a58dece61f076c356f7210baf480
mohamed <mohamedaliaboshosha927@gmail.com>
2026-05-02 17:11:34 -0700

    feat(router): add onboarding route
    Register the onboarding view route within the application router to enable navigation to the onboarding flow.

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,13 +1,19 @@
+import 'package:furniture/features/onboarding/presentation/views/onboarding_view.dart';
 import 'package:go_router/go_router.dart';
 
 import '../../features/splash/presentation/views/splash_view.dart';
 
 class AppRouter {
+  static const kOnboardingView = '/onboardingView';
   static GoRouter router = GoRouter(
     routes: [
       GoRoute(
         path: '/',
         builder: (context, state) => const SplashView(),
+      ),
+      GoRoute(
+        path: kOnboardingView,
+        builder: (context, state) => const OnboardingView(),
       ),
     ],
   );
