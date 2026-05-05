cbf60ea304a3ca2cf04af8498d380f3f0e3831b7
mohamed <mohamedaliaboshosha927@gmail.com>
2026-05-05 15:10:41 -0700

    feat(router): add route for forget password view
    Register the forget password view route in the app router to enable
    navigation to the password reset flow.

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -2,6 +2,7 @@ import 'package:furniture/features/auth/sign_up/presentation/views/sign_up_view.
 import 'package:furniture/features/onboarding/presentation/views/onboarding_view.dart';
 import 'package:go_router/go_router.dart';
 
+import '../../features/auth/forget_password/presentation/views/forget_password_view.dart';
 import '../../features/auth/login/presentation/views/login_view.dart';
 import '../../features/splash/presentation/views/splash_view.dart';
 
@@ -9,6 +10,7 @@ class AppRouter {
   static const kOnboardingView = '/onboardingView';
   static const kLoginView = '/loginView';
   static const kSignUpView = '/signUpView';
+  static const kForgetPasswordView = '/forgetPasswordView';
   static GoRouter router = GoRouter(
     routes: [
       GoRoute(
@@ -26,6 +28,10 @@ class AppRouter {
       GoRoute(
         path: kSignUpView,
         builder: (context, state) => const SignUpView(),
+      ),
+      GoRoute(
+        path: kForgetPasswordView,
+        builder: (context, state) => const ForgetPasswordView(),
       ),
     ],
   );
