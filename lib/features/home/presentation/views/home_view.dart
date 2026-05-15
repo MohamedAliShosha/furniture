62890cdc539a1623fcf767bc07101d9044800e3e
mohamed <mohamedaliaboshosha927@gmail.com>
2026-05-14 23:26:51 -0700

    refactor(home): extract home view body to HomeViewContent
    Remove the private `_HomeView` class from `home_view.dart` and replace it with the `HomeViewContent` widget to improve code organization and separation of concerns.

--- lib/features/home/presentation/views/home_view.dart
+++ lib/features/home/presentation/views/home_view.dart
@@ -4,12 +4,10 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get_it/get_it.dart';
 
 import '../../data/repositories/product_repo.dart';
-import '../cubit/all_products/all_products_state.dart';
 import '../cubit/featured_items/featured_items_cubit.dart';
 import '../cubit/all_products/all_products_cubit.dart';
-import '../cubit/featured_items/featured_items_state.dart';
 import '../cubit/products_by_category/products_by_category_cubit.dart';
-import '../widgets/home_view_body.dart';
+import '../widgets/home_view_content.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
@@ -42,46 +40,8 @@ class HomeView extends StatelessWidget {
                 ProductsByCategoryCubit(GetIt.instance<ProductRepo>()),
           ),
         ],
-        child: const _HomeView(),
+        child: const HomeViewContent(),
       ),
-    );
-  }
-}
-
-class _HomeView extends StatelessWidget {
-  const _HomeView();
-
-  @override
-  Widget build(BuildContext context) {
-    return BlocBuilder<FeaturedItemsCubit, FeaturedItemsState>(
-      builder: (context, state) {
-        if (state is FeaturedItemsLoading) {
-          return const Center(child: CircularProgressIndicator());
-        } else if (state is FeaturedItemsSuccess) {
-          return BlocBuilder<AllProductsCubit, AllProductsState>(
-            builder: (context, allProductsState) {
-              if (allProductsState is AllProductsLoading) {
-                return const Center(child: CircularProgressIndicator());
-              } else if (allProductsState is AllProductsSuccess) {
-                return HomeViewBody(
-                  featuredProducts: state.products,
-                  allProducts: allProductsState.products,
-                );
-              } else {
-                return HomeViewBody(
-                  featuredProducts: state.products,
-                  allProducts: const [], // fallback
-                );
-              }
-            },
-          );
-        } else {
-          return const HomeViewBody(
-            featuredProducts: [],
-            allProducts: [],
-          ); // Initial state
-        }
-      },
     );
   }
 }
