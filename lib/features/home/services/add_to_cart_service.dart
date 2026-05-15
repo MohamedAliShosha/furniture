8517932b3bb945d39e44cd8a9a0beb7e66e893de
mohamed <mohamedaliaboshosha927@gmail.com>
2026-05-14 20:42:23 -0700

    feat(home): add cart and wishlist management services
    Implement AddToCartService to handle cart item lifecycle, including
    adding, removing, updating quantities, and calculating totals.
    Implement WishListService to manage product favorites using a Set
    for efficient lookups and toggling.

---
+++ lib/features/home/services/add_to_cart_service.dart
@@ -0,0 +1,59 @@
+import '../data/models/cart_item.dart';
+
+class AddToCartService {
+  // Will store the ids of the products that are added to the whishList
+  final List<CartItem> cartItems = [];
+
+  // Returns the the cart items
+  List<CartItem> getCartItems() => cartItems;
+
+  // Returns the total price of the cart
+  double get total => cartItems.fold(0, (sum, item) => sum + item.totalPrice);
+
+  // Returns the number of items in the cart
+  int getCartItemsCount() => cartItems.length;
+
+  // Adds a product to the cart
+  void addToCart(CartItem cartItem) {
+    // Getting the index of the product in the cart
+    final existingIndex = cartItems.indexWhere(
+      (item) =>
+          item.product.id == cartItem.product.id &&
+          item.selectedColor == cartItem.selectedColor,
+    );
+    // if the product is already in the cart
+    if (existingIndex >= 0) {
+      // update the quantity by adding the new quantity
+      cartItems[existingIndex].quantity += cartItem.quantity;
+    } else {
+      // if the product is not in the cart then add it
+      cartItems.add(cartItem);
+    }
+  }
+
+  // Removes a product from the cart
+  void removeFromCart(String productId, {String? selectedColor}) {
+    if (selectedColor != null) {
+      cartItems.removeWhere((item) =>
+          item.product.id == productId && item.selectedColor == selectedColor);
+    } else {
+      cartItems.removeWhere((item) => item.product.id == productId);
+    }
+  }
+
+  void updateQuantity(String productId, int quantity) {
+    final index = cartItems.indexWhere((item) => item.product.id == productId);
+    // if the product is in the cart
+    if (index >= 0) {
+      // if the quantity is less than or equal to 0 then remove the item
+      if (quantity <= 0) {
+        cartItems.removeAt(index);
+      } else {
+        // update the quantity if the quantity is greater than 0
+        cartItems[index].quantity = quantity;
+      }
+    }
+  }
+
+  void clearCart() => cartItems.clear();
+}
