4bc03b307c3206c3483760fab4ded5eec39696df
Mohamed Ali <mohamedaliaboshosha927@gmail.com>
2026-05-23 23:51:16 -0700

    feat(wishlist): add empty state widget
    Add EmptyWisListWidget to provide visual feedback when the wishlist
    contains no items.

---
+++ lib/features/cart/presentation/widgets/empty_wishlist_widget.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:furniture/core/utils/constants.dart';
+import 'package:gap/gap.dart';
+
+class EmptyWisListWidget extends StatelessWidget {
+  const EmptyWisListWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(
+            Icons.favorite_border,
+            size: 64,
+            color: Colors.grey[400],
+          ),
+          const Gap(16),
+          Text(
+            'Your wishlist is empty',
+            style: AppConstants.titleStyle.copyWith(
+              color: Colors.grey[600],
+            ),
+          ),
+          const Gap(8),
+          Text(
+            textAlign: TextAlign.center,
+            'Save items you like by tapping the heart icon',
+            style: AppConstants.bodyStyle.copyWith(
+              color: Colors.grey,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
