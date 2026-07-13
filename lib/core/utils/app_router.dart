f3a169a120940223ddac90017b846607dd4d6eb8
Mohamed Ali <mohamedaliaboshosha927@gmail.com>
2026-07-13 15:32:35 -0700

    feat(router): add product details route
    Add a new route for product details view and include the necessary
    imports and route configuration to handle passing ProductModel via
    extra parameters.

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -3,7 +3,9 @@ import 'package:go_router/go_router.dart';
 import '../../features/auth/forget_password/presentation/views/forget_password_view.dart';
 import '../../features/auth/login/presentation/views/login_view.dart';
 import '../../features/auth/sign_up/presentation/views/sign_up_view.dart';
+import '../../features/home/data/models/product_model.dart';
 import '../../features/onboarding/presentation/views/onboarding_view.dart';
+import '../../features/product_details/presentation/views/product_details_view.dart';
 import '../../features/splash/presentation/views/splash_view.dart';
 import '../../main_view.dart';
 
@@ -12,6 +14,7 @@ class AppRouter {
   static const kLoginView = '/loginView';
   static const kSignUpView = '/signUpView';
   static const kMainView = '/mainView';
+  static const kProductDetailsView = '/productDetailsView';
   static const kForgetPasswordView = '/forgetPasswordView';
 
   static GoRouter router = GoRouter(
@@ -39,6 +42,16 @@ class AppRouter {
       GoRoute(
         path: kMainView,
         builder: (context, state) => const MainView(),
+      ),
+      GoRoute(
+        path: kProductDetailsView,
+        builder: (context, state) {
+          final product = state.extra as ProductModel;
+          return ProductDetailsView(
+            // Pass the product data to the ProductDetailsView if needed
+            product: product,
+          );
+        },
       ),
     ],
   );
