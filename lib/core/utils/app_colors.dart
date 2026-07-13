28842502a8495f9e257f266ac3b8810164603773
Mohamed Ali <mohamedaliaboshosha927@gmail.com>
2026-07-12 19:02:25 -0700

    feat(core): add AppColors utility class

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  static const Color primary = Color(0xFFFFB800);
+  static const Color secondaryText = Color(0xFF1A1A1A);
+  static const Color softGrey = Color(0xFFF5F5F5);
+  static const Color grey = Colors.grey;
+  static const Color black = Colors.black;
+  static const Color red = Colors.red;
+  static const Color white = Colors.white;
+}
