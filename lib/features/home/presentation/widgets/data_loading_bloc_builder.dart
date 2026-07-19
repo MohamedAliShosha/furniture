0fc71a0bfc30f75e5f35b93b840e09fc70eebd57
Mohamed Ali <mohamedaliaboshosha927@gmail.com>
2026-07-18 19:34:39 -0700

    refactor(home): integrate products_by_category cubit into data loading builder
    Wrap the existing BlocBuilders within a new BlocBuilder for
    ProductsByCategoryCubit to allow the home view to access category-specific
    product states. This enables the UI to react to products filtered by
    category alongside existing featured and all-products states.

--- lib/features/home/presentation/widgets/data_loading_bloc_builder.dart
+++ lib/features/home/presentation/widgets/data_loading_bloc_builder.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:furniture/core/utils/app_router.dart';
+import 'package:furniture/core/utils/app_texts.dart';
+import 'package:go_router/go_router.dart';
 import '../cubit/all_products/all_products_state.dart';
 import '../cubit/all_products/all_products_cubit.dart';
 import '../cubit/featured_items/featured_items_cubit.dart';
 import '../cubit/featured_items/featured_items_state.dart';
 import '../cubit/categories/categories_cubit.dart';
 import '../cubit/categories/categories_state.dart';
+import '../cubit/products_by_category/products_by_category_cubit.dart';
+import '../cubit/products_by_category/products_by_category_state.dart';
 import '../widgets/home_view_body.dart';
 
 class DataLoadingBlocBuilder extends StatefulWidget {
@@ -26,31 +31,61 @@ class _DataLoadingBlocBuilderState extends State<DataLoadingBlocBuilder> {
           builder: (context, allProductsState) {
             return BlocBuilder<CategoriesCubit, CategoriesState>(
               builder: (context, categoriesState) {
-                final featuredProducts = (featuredState is FeaturedItemsSuccess)
-                    ? featuredState.products
-                    : <dynamic>[];
-                final allProducts = (allProductsState is AllProductsSuccess)
-                    ? allProductsState.products
-                    : <dynamic>[];
-                final categories = (categoriesState is CategoriesSuccess)
-                    ? categoriesState.categories
-                    : <dynamic>[];
-                final isFeaturedLoading = featuredState is FeaturedItemsLoading;
-                final isAllProductsLoading = allProductsState is AllProductsLoading;
-                final isCategoriesLoading = categoriesState is CategoriesLoading;
+                return BlocBuilder<ProductsByCategoryCubit,
+                    ProductsByCategoryState>(
+                  builder: (context, productsByCategoryState) {
+                    final featuredProducts =
+                        (featuredState is FeaturedItemsSuccess)
+                            ? featuredState.products
+                            : <dynamic>[];
+                    final allProducts = (allProductsState is AllProductsSuccess)
+                        ? allProductsState.products
+                        : <dynamic>[];
+                    final categories = (categoriesState is CategoriesSuccess)
+                        ? categoriesState.categories
+                        : <dynamic>[];
+                    final isFeaturedLoading =
+                        featuredState is FeaturedItemsLoading;
+                    final isAllProductsLoading =
+                        allProductsState is AllProductsLoading;
+                    final isCategoriesLoading =
+                        categoriesState is CategoriesLoading;
+                    final productsByCategory =
+                        (productsByCategoryState is ProductsByCategorySuccess)
+                            ? productsByCategoryState.products
+                            : <dynamic>[];
+                    final isProductsByCategoryLoading =
+                        productsByCategoryState is ProductsByCategoryLoading;
 
-                return HomeViewBody(
-                  featuredProducts: featuredProducts,
-                  allProducts: allProducts,
-                  categories: categories,
-                  isFeaturedLoading: isFeaturedLoading,
-                  isAllProductsLoading: isAllProductsLoading,
-                  isCategoriesLoading: isCategoriesLoading,
-                  selectedCategory: _selectedCategory,
-                  onCategoryTap: (category) {
-                    setState(() {
-                      _selectedCategory = category;
-                    });
+                    return HomeViewBody(
+                      featuredProducts: featuredProducts,
+                      allProducts: allProducts,
+                      categories: categories,
+                      isFeaturedLoading: isFeaturedLoading,
+                      isAllProductsLoading: isAllProductsLoading,
+                      isCategoriesLoading: isCategoriesLoading,
+                      productsByCategory: productsByCategory,
+                      isProductsByCategoryLoading: isProductsByCategoryLoading,
+                      selectedCategory: _selectedCategory,
+                      onCategoryTap: (category) {
+                        if (category == AppTexts.allCategory) {
+                          context
+                              .read<ProductsByCategoryCubit>()
+                              .fetchProductsByCategory(AppTexts.allCategory);
+                        } else {
+                          context
+                              .read<ProductsByCategoryCubit>()
+                              .fetchProductsByCategory(category);
+                          GoRouter.of(context).push(
+                            AppRouter.kCategoryView,
+                            extra: category,
+                          );
+                        }
+                        setState(() {
+                          _selectedCategory = category;
+                        });
+                      },
+                    );
                   },
                 );
               },
