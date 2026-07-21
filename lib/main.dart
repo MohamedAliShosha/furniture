b3e3970b2fea43a015df1fd3dff80aba340b78d8
Mohamed Ali <mohamedaliaboshosha927@gmail.com>
2026-07-20 18:03:57 -0700

    feat(special_offer): provide special offer cubit via BlocProvider
    Register the SpecialOfferCubit in the main application widget tree
    to ensure the state management layer is accessible to the special
    offers feature components.

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ import 'features/blog/presentation/cubit/blog_cubit.dart';
 import 'features/cart/presentation/cubit/cart_cubit.dart';
 import 'features/wishlist/presentation/cubit/wishlist_cubit.dart';
 import 'features/home/presentation/cubit/user/user_cubit.dart';
+import 'features/special_offer/presentation/cubit/special_offer_cubit.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -45,6 +46,9 @@ class Furniture extends StatelessWidget {
         ),
         BlocProvider(
           create: (context) => getIt<WishlistCubit>(),
+        ),
+        BlocProvider(
+          create: (context) => getIt<SpecialOfferCubit>(),
         ),
       ],
       child: MaterialApp.router(
