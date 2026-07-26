4aec2860fed6c35c0036f2425dacb9d7aaccc1cc
Mohamed Ali <mohamedaliaboshosha927@gmail.com>
2026-07-25 17:55:15 -0700

    feat(orders): add OrderStatusEnum
    Define the possible states for an order, including confirmed,
    processing, shipped, outForDelivery, delivered, and cancelled.

---
+++ lib/features/orders/data/enums/order_status_enum.dart
@@ -0,0 +1,9 @@
+// contains order status like confirmed, processing, shipped, outOfDelivery, delivered, cancelled
+enum OrderStatusEnum {
+  confirmed,
+  processing,
+  shipped,
+  outForDelivery,
+  delivered,
+  cancelled,
+}
