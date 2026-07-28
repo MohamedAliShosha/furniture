a38b85382ea27cafac5b21e139345c37af3bc1e4
Mohamed Ali <mohamedaliaboshosha927@gmail.com>
2026-07-27 18:56:04 -0700

    feat(shipping_address): add view body widget
    Introduce `ShippingAddressesViewBody` to encapsulate the main content
    area of the shipping addresses screen, delegating state management
    to the `ShippingAddressViewBlocBuilder`.

---
+++ lib/features/shipping_address/presentation/widgets/shipping_addresses_view_body.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+import 'shipping_address_view_bloc_builder.dart';
+
+class ShippingAddressesViewBody extends StatelessWidget {
+  const ShippingAddressesViewBody({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const ShippingAddressViewBlocBuilder();
+  }
+}
