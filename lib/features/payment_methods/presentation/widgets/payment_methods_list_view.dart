4b99df1819ccdc45a8fa56e1ea73ebf59ea7d4d5
Mohamed Ali <mohamedaliaboshosha927@gmail.com>
2026-07-30 09:59:29 -0700

    feat(payment_methods): add list view widget for displaying methods
    Create PaymentMethodsListView to render a scrollable list of
    PaymentMethodModel items using a ListView.builder. This component
    integrates PaymentMethodCardWidget to display individual payment
    method details within the payment management screen.

---
+++ lib/features/payment_methods/presentation/widgets/payment_methods_list_view.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+import '../../../../core/utils/constants.dart';
+import '../../../../features/payment_methods/data/models/payment_method_model.dart';
+import 'payment_method_card_widget.dart';
+
+class PaymentMethodsListView extends StatelessWidget {
+  const PaymentMethodsListView({
+    super.key,
+    required this.paymentMethods,
+  });
+
+  final List<PaymentMethodModel> paymentMethods;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      padding: const EdgeInsets.all(AppConstants.defaultPadding),
+      itemCount: paymentMethods.length,
+      itemBuilder: (context, index) {
+        final method = paymentMethods[index];
+        return PaymentMethodCardWidget(method: method);
+      },
+    );
+  }
+}
