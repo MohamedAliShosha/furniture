20baf358136187d111b353d1c000cec502631f6e
Mohamed Ali <mohamedaliaboshosha927@gmail.com>
2026-08-13 14:25:40 -0700

    feat(about_us): add social link model

---
+++ lib/features/about_us/data/models/social_link_model.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SocialLinkModel {
+  final IconData icon;
+  final String name;
+  final String url;
+
+  SocialLinkModel({
+    required this.icon,
+    required this.name,
+    required this.url,
+  });
+}
