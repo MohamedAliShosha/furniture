d98690b7c569fb17af0c69b0787139eb271a55bc
Mohamed Ali <mohamedaliaboshosha927@gmail.com>
2026-08-23 14:54:51 -0700

    feat(profile): refresh user data on view initialization

--- lib/features/profile/presentation/views/profile_view.dart
+++ lib/features/profile/presentation/views/profile_view.dart
@@ -1,8 +1,21 @@
 import 'package:flutter/material.dart';
 import '../widgets/profile_view_body.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '../../../home/presentation/cubit/user/user_cubit.dart';
 
-class ProfileView extends StatelessWidget {
+class ProfileView extends StatefulWidget {
   const ProfileView({super.key});
+
+  @override
+  State<ProfileView> createState() => _ProfileViewState();
+}
+
+class _ProfileViewState extends State<ProfileView> {
+  @override
+  void initState() {
+    super.initState();
+    context.read<UserCubit>().refreshUser();
+  }
 
   @override
   Widget build(BuildContext context) {
