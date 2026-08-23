51ebe89aa517d23890a86a7d55e68919b50212d0
Mohamed Ali <mohamedaliaboshosha927@gmail.com>
2026-08-23 14:55:31 -0700

    refactor(home): integrate user cubit into home header
    Migrate home header user data retrieval from direct UserService access
    to BlocBuilder using UserCubit to ensure reactive UI updates based
    on user state.

--- lib/features/home/presentation/widgets/home_header.dart
+++ lib/features/home/presentation/widgets/home_header.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../services/user_service.dart';
 import '../../../../core/utils/app_images.dart';
 import '../../../../core/utils/service_locator.dart';
+import '../cubit/user/user_cubit.dart';
+import '../cubit/user/user_state.dart';
 
 import '../../../../core/utils/app_texts.dart';
 import '../../../../core/utils/constants.dart';
@@ -11,47 +14,52 @@ class HomeHeader extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final UserService userService = getIt<UserService>();
-    final user = userService.user;
-    return Padding(
-      padding: const EdgeInsets.symmetric(
-        horizontal: 16,
-        vertical: 8,
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+    return BlocBuilder<UserCubit, UserState>(
+      builder: (context, state) {
+        final userService = getIt<UserService>();
+        final user = state is UserSuccess ? state.user : userService.user;
+
+        return Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 16,
+            vertical: 8,
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Text(
-                AppTexts.hello,
-                style: AppConstants.bodyStyle.copyWith(
-                  color: Colors.grey,
-                ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    AppTexts.hello,
+                    style: AppConstants.bodyStyle.copyWith(
+                      color: Colors.grey,
+                    ),
+                  ),
+                  Text(
+                    user?.name ?? AppTexts.guestUser,
+                    style: AppConstants.headingStyle,
+                  ),
+                ],
               ),
-              Text(
-                user?.name ?? userService.user?.name ?? AppTexts.guestUser,
-                style: AppConstants.headingStyle,
+              GestureDetector(
+                onTap: () {
+                  // navigate to account info screen
+                },
+                child: CircleAvatar(
+                  backgroundColor: Colors.grey[200],
+                  radius: 20,
+                  backgroundImage: user?.profileImage != null
+                      ? NetworkImage(user!.profileImage!)
+                      : const AssetImage(
+                          AppImages.profileImage,
+                        ),
+                ),
               ),
             ],
           ),
-          GestureDetector(
-            onTap: () {
-              // navigate to account info screen
-            },
-            child: CircleAvatar(
-              backgroundColor: Colors.grey[200],
-              radius: 20,
-              backgroundImage: user?.profileImage != null
-                  ? NetworkImage(user!.profileImage!)
-                  : const AssetImage(
-                      AppImages.profileImage,
-                    ),
-            ),
-          ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
