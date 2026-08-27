5440f07b2803d629b7bf8138926525dc9be1bba9
Mohamed Ali <mohamedaliaboshosha927@gmail.com>
2026-08-27 14:53:43 -0700

    refactor(auth): update DatabaseService interface for CRUD and filtering
    Expand the DatabaseService abstract class to include updateData and
    deleteData methods. Also, update the getData signature to support
    a 'where' parameter for advanced querying. Removed redundant comments
    to clean up the interface definition.

--- lib/features/auth/services/database_service.dart
+++ lib/features/auth/services/database_service.dart
@@ -1,7 +1,6 @@
 // This class will contain all the methods that will be used to interact with the database => Read, Write, Delete, Update
 
 abstract class DatabaseService {
-  // path => could be an endpoint or a collection name
   Future<void> addData({
     required String path,
     required Map<String, dynamic> data,
@@ -11,10 +10,23 @@ abstract class DatabaseService {
   Future<dynamic> getData({
     required String path,
     String? documentId,
-    // used to filter the data we will filter products based on these query parameters
     Map<String, dynamic>? query,
+    Map<String, dynamic>? where,
   });
 
-  Future<bool> checkIfDataExists(
-      {required String path, required String documentId});
+  Future<bool> checkIfDataExists({
+    required String path,
+    required String documentId,
+  });
+
+  Future<void> updateData({
+    required String path,
+    required String documentId,
+    required Map<String, dynamic> data,
+  });
+
+  Future<void> deleteData({
+    required String path,
+    required String documentId,
+  });
 }
